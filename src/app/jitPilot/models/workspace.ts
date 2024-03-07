49b43e417ddc9ad3594ee5d7818a1714d32745d6
unknown <fatimazahraboudad9@gmail.com>
2024-03-07 23:48:45 +0100

    feature workspace

--- src/app/jitPilot/models/workspace.ts
+++ src/app/jitPilot/models/workspace.ts
@@ -1,2 +1,6 @@
-export interface Workspace {
+export interface workspace{
+
+    workspaceId : number;
+    name : string;
+    description : string;
 }
--- src/app/jitPilot/services/workspace.service.ts
+++ src/app/jitPilot/services/workspace.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { workspace } from '../models/workspace';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkspaceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllWorkspace(): Observable<workspace[]> {
+    return this.http.get<workspace[]>(`http://localhost:8080/api/workspaces`);
+  }
+
+  deleteWorkspace(id: number): Observable<workspace> {
+    return this.http.delete<workspace>(`http://localhost:8080/api/workspaces/${id}`);
+  }
+
+
+  getWorkspaceById(id: number): Observable<workspace> {
+    return this.http.get<workspace>(`http://localhost:8080/api/workspaces/${id}`);
+  }
+
+  create(data: workspace): Observable<workspace> {
+    return this.http.post<workspace>(`http://localhost:8080/api/workspaces`, data);
+  }
+
+  update(id: number, data: workspace): Observable<workspace> {
+    return this.http.put<workspace>(`http://localhost:8080/api/workspaces/${id}`, data);
+  }
+
+
 }
